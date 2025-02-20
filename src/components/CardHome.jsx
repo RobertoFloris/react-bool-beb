37d4761de57ee733d766f04bcdf827425e90c63c
SimoneZampieri <simonezampieri106@gmail.com>
2025-02-20 14:15:59 +0100

    fix: update like feature in CardHome component to use PATCH method and improved error handling

--- src/components/CardHome.jsx
+++ src/components/CardHome.jsx
@@ -1,22 +1,30 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { useState } from "react";
 
 const CardHome = ({ bnb }) => {
-  const { id, description, address } = bnb
+  const { id, description, address } = bnb;
   const [likes, setLikes] = useState(0);
+  const apiUrl = import.meta.env.VITE_API_URL;
 
   const likeHandler = () => {
-    fetch("/updateLikes", {
-      method: "POST",
+    fetch(`${apiUrl}/${id}`, {
+      method: "PATCH",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ id: bnb.id }),
     })
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response not ok");
+        }
+        return response.json();
+      })
       .then((data) => {
-        if (data.success) {
-          setLikes(data.likes);
+        console.log("Response data:", data); // Log the response data
+        if (data.message) {
+          // Assuming the server increments the likes and returns a message
+          setLikes((prevLikes) => prevLikes + 1);
         } else {
           console.error("Errore nell'aggiornamento dei like");
         }
