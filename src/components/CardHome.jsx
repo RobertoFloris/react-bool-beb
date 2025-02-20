6fc108e2c277e1301c6f41bb4073b958df338e69
SimoneZampieri <simonezampieri106@gmail.com>
2025-02-20 12:01:30 +0100

    [add] implement like feature in CardHome component

--- src/components/CardHome.jsx
+++ src/components/CardHome.jsx
@@ -1,16 +1,42 @@
+import { useState } from "react";
+
 const CardHome = ({ bnb }) => {
-  const { description, address } = bnb
+  const { description, address } = bnb;
+  const [likes, setLikes] = useState(0);
+
+  const likeHandler = () => {
+    fetch("/updateLikes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id: bnb.id }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          setLikes(data.likes);
+        } else {
+          console.error("Errore nell'aggiornamento dei like");
+        }
+      })
+      .catch((errror) => {
+        console.error("Errore", error);
+      });
+  };
   return (
     <div>
       <ul>
         <li>
           <p>{description}</p>
           <p>{address}</p>
+          <button onClick={likeHandler}>Like</button>
+          <p>Likes: {likes}</p>
           <hr />
         </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default CardHome
+export default CardHome;
