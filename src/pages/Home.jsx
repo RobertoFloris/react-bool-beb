8ca2426df726d140ac9d8568688e0f29dc11918d
Lucia-Susanna <lucia_susanna@hotmail.it>
2025-02-26 17:30:55 +0100

    fix: pagination buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -92,13 +92,12 @@ const Home = () => {
           </h2>
         )}
       </div>
-      <div className="flex justify-center items-center mt-20 my-2 gap-4">
+      {bnb.length > 0 && <div className="flex justify-center items-center mt-20 my-2 gap-4">
         <button
           onClick={prevPage}
           disabled={currentPage === 1}
-          className={`px-4 py-2 bg-gray-300 rounded disabled:opacity-50 ${
-            currentPage > 1 ? "cursor-pointer" : "cursor-default"
-          }`}
+          className={`px-4 py-2 bg-gray-300 rounded disabled:opacity-50 ${currentPage > 1 ? "cursor-pointer" : "cursor-default"
+            }`}
         >
           Pagina Precedente
         </button>
@@ -110,13 +109,12 @@ const Home = () => {
         <button
           onClick={nextPage}
           disabled={currentPage === totalPages}
-          className={`px-4 py-2 bg-gray-300 rounded disabled:opacity-50 ${
-            currentPage < totalPages ? "cursor-pointer" : "cursor-default"
-          }`}
+          className={`px-4 py-2 bg-gray-300 rounded disabled:opacity-50 ${currentPage < totalPages ? "cursor-pointer" : "cursor-default"
+            }`}
         >
           Pagina Successiva
         </button>
-      </div>
+      </div>}
     </>
   );
 };
