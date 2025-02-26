dff078fd8589fd159f1c39743cd6e3f0d5e38577
Lucia-Susanna <lucia_susanna@hotmail.it>
2025-02-26 17:25:13 +0100

    add: outline star

--- src/components/CardHome.jsx
+++ src/components/CardHome.jsx
@@ -35,12 +35,20 @@ const CardHome = ({ bnb }) => {
           </svg>
         </button>
       </div>
-      <Link to={`/${id}`} className="flex-grow p-4">
+      <Link to={`/${id}`} className="flex-grow p-4 pb-0">
         <h3 className="font-bold">{description}</h3>
-        <p className="text-gray-500">{likes} ❤️</p>
+        <p className="text-gray-500 flex">{likes} <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 text-red-500 ms-3">
+          <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+        </svg>
+        </p>
         <p className="text-gray-500">{city}</p>
       </Link>
-      <StarRating rating={avg_vote} />
+
+      <div className="p-4">
+        <StarRating vote={avg_vote} />
+
+      </div>
+
       {/* <div className="flex justify-between p-2">
         <Link
           to={`/${id}`}
