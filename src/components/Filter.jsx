717b76882841a84e3703a2c3958fc33141750ee6
Lucia-Susanna <lucia_susanna@hotmail.it>
2025-02-26 10:41:45 +0100

    feat: reset filter button

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useGlobalContext } from "../context/GlobalContext";
+import Counter from "./Counter";
 
 const Filter = ({ deleteFilter }) => {
   const { fetchBnB } = useGlobalContext();
@@ -21,8 +22,8 @@ const Filter = ({ deleteFilter }) => {
         <div className="">
           <h3 className="text-center">Filtri</h3>
           <button onClick={deleteFilter} className="absolute end-5 top-5">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg>
 
           </button>
@@ -37,6 +38,8 @@ const Filter = ({ deleteFilter }) => {
               value={city}
               onChange={(e) => setCity(e.target.value)}
             />
+
+
           </div>
 
           <div className="border-b border-gray-900/10 pb-3">
