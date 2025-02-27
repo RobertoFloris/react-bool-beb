7c13db77761fc5b66726c84603439acf5fde5440
SimoneZampieri <simonezampieri106@gmail.com>
2025-02-27 09:14:07 +0100

    fix: update back link with SVG icon

--- src/components/CardDetails.jsx
+++ src/components/CardDetails.jsx
@@ -25,7 +25,7 @@ const CardDetails = ({ bnbId }) => {
     host_phone,
     thumbnail,
     reviews,
-    handleFilter
+    handleFilter,
   } = bnbId;
 
   return (
@@ -35,7 +35,20 @@ const CardDetails = ({ bnbId }) => {
         to={-1}
         onClick={handleFilter}
       >
-        Torna alla Home
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
+          />
+        </svg>
       </Link>
       <div className="max-w-4xl mx-auto p-4 bg-white rounded-lg shadow-lg">
         <img
