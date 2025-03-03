53baabf7a52a00b4d78c40b8ddccd07420362876
Lucia-Susanna <lucia_susanna@hotmail.it>
2025-03-03 10:46:46 +0100

    fix: homepage responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className=" h-25 z-2 flex justify-between items-center px-4 text-neutral-50 bg-stone-700 ">
+    <header className="h-25 px-10 flex justify-between items-center px-4 text-neutral-50 bg-stone-700 ">
       <Link to="/">
         <img src="./img/logo_b&b.png" className="h-25" alt="" />
       </Link>
       <div className="flex items-center space-x-4">
-        <Link to="/nuova-casa" className="p-2">
+        <Link to="/nuova-casa" className="px-2 py-2 ms-1 text-xs text-stone-600 bg-yellow-500 border-stone-400 rounded-xl cursor-pointer">
           Affitta su BoolB&B
         </Link>
       </div>
