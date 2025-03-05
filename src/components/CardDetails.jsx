08a9607435fd48beac5c2af149435b60aeab4a7b
Lucia-Susanna <lucia_susanna@hotmail.it>
2025-03-05 01:32:55 +0100

    update: card detail-style

--- src/components/CardDetails.jsx
+++ src/components/CardDetails.jsx
@@ -107,19 +107,31 @@ const CardDetails = ({ bnbId }) => {
       });
   };
 
+  const services = [
+    { name: "WiFi", available: wifi },
+    { name: "TV", available: tv },
+    { name: "Piscina", available: pool },
+    { name: "Cucina", available: kitchen },
+    { name: "Lavatrice", available: washing_machine },
+    { name: "Riscaldamento", available: heating },
+    { name: "Aria condizionata", available: air_conditioning },
+    { name: "Asciugacapelli", available: hairdryer },
+    { name: "Ferro da stiro", available: iron },
+  ];
+
   return (
     <div className="relative">
-      <Link className="static top-30 left-4 mx-10 mt-10" to={-1} onClick={handleFilter}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 bg-stone-400 text-white rounded cursor-pointer">
+      <Link className="top-30 mx-10 mt-10" to={-1} onClick={handleFilter}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 bg-stone-400 text-white mx-5 rounded cursor-pointer">
           <path strokeLinecap="round" strokeLinejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
         </svg>
       </Link>
       <div className="flex flex-col md:flex-row items-center mt-10 mx-4 md:mx-20">
         <ImageSlider thumbnail={thumbnail} foto1={foto1} foto2={foto2} />
         <div className="p-4 m-5 w-full md:w-1/3 h-full flex flex-col justify-between">
-          <h2 className="text-2xl font-bold mb-2">{description}</h2>
+          <h1 className="text-3xl font-bold mb-4 text-left">{description}</h1>
           <div className="flex flex-col">
-            <p className="text-gray-700 mb-2"><strong>Tipo:</strong> {type}</p>
+            <p className="text-gray-700 mb-2"><strong>Tipo di abitazione:</strong> {type}</p>
             <p className="text-gray-700 mb-2"><strong>Tipo di alloggio:</strong> {accomodation_type}</p>
             <p className="text-gray-700 mb-2"><strong>Città:</strong> {city}</p>
             <p className="text-gray-700 mb-2"><strong>Indirizzo:</strong> {address}</p>
@@ -128,45 +140,54 @@ const CardDetails = ({ bnbId }) => {
             <p className="text-gray-700 mb-2"><strong>Camere:</strong> {rooms}</p>
             <p className="text-gray-700 mb-2"><strong>Bagni:</strong> {restrooms}</p>
             <p className="text-gray-700 mb-2"><strong>Metri Quadrati:</strong> {square_meters}</p>
-            <p className="text-gray-700 mb-2"><strong>Prezzo:</strong> {price ? `${price} € a notte` : "Non disponibile"}</p>
-            <p className="text-gray-700 mb-2"><strong>Likes:</strong> {likes} ❤️</p>
-            <p className="text-gray-700 mb-2"><strong>Host:</strong> {host_name} {host_surname}</p>
-            <p className="text-gray-700 mb-2"><strong>Email Host:</strong> {host_email}</p>
-            <p className="text-gray-700 mb-2"><strong>Telefono Host:</strong> {host_phone}</p>
-            <p className="text-gray-700 mb-2"><strong>Servizi:</strong></p>
-            <ul className="list-disc list-inside">
-              <li className="text-gray-700 mb-2">WiFi: {wifi ? "Sì" : "No"}</li>
-              <li className="text-gray-700 mb-2">TV: {tv ? "Sì" : "No"}</li>
-              <li className="text-gray-700 mb-2">Piscina: {pool ? "Sì" : "No"}</li>
-              <li className="text-gray-700 mb-2">Cucina: {kitchen ? "Sì" : "No"}</li>
-              <li className="text-gray-700 mb-2">Lavatrice: {washing_machine ? "Sì" : "No"}</li>
-              <li className="text-gray-700 mb-2">Riscaldamento: {heating ? "Sì" : "No"}</li>
-              <li className="text-gray-700 mb-2">Aria condizionata: {air_conditioning ? "Sì" : "No"}</li>
-              <li className="text-gray-700 mb-2">Asciugacapelli: {hairdryer ? "Sì" : "No"}</li>
-              <li className="text-gray-700 mb-2">Ferro da stiro: {iron ? "Sì" : "No"}</li>
-            </ul>
+            <div className="flex items-center mt-2">
+              <button className="px-2 py-1 border-stone-400 rounded-xl cursor-pointer">
+                ❤️
+              </button>
+              <span className="ml-2 text-gray-700">{likes}</span>
+            </div>
           </div>
         </div>
       </div>
-      <div className="flex items-center justify-between mt-10 mb-3 mx-20">
-        <h3 className="text-xl font-bold">Recensioni:</h3>
-        <button
-          onClick={() => setIsModalOpen(true)}
-          className="px-2 py-2 ms-1 text-xs text-stone-600 bg-yellow-500 border-stone-400 rounded-xl cursor-pointer"
-        >
-          Aggiungi una recensione
-        </button>
+      <div className="mx-4 md:mx-20 mt-10">
+        <h3 className="text-2xl font-bold mb-4">Servizi Offerti:</h3>
+        <ul className="list-none flex flex-wrap">
+          {services.filter(service => service.available).map((service, index) => (
+            <li key={index} className="flex items-center mb-2 mr-4">
+              <span className="text-green-500 mr-2">✔</span>
+              <span className="ml-2">{service.name}</span>
+            </li>
+          ))}
+        </ul>
+        <h3 className="text-2xl font-bold mt-6">Prezzo: {price ? `${price} € a notte` : "Non disponibile"}</h3>
       </div>
-      <div className="flex flex-col m-10">
-        {reviews?.length > 0 ? (
-          reviews.map((review) => (
-            <div className="m-2 w-full" key={review.id}>
-              <ReviewCard review={review} />
-            </div>
-          ))
-        ) : (
-          <p className="text-gray-700">Nessuna recensione disponibile</p>
-        )}
+      <div className="mx-4 md:mx-20 mt-10 p-4 border-t border-gray-300">
+        <h3 className="text-2xl font-bold mb-4">Dati dell'Host:</h3>
+        <p className="text-gray-700 mb-2"><strong>Host:</strong> {host_name} {host_surname}</p>
+        <p className="text-gray-700 mb-2"><strong>Email Host:</strong> {host_email}</p>
+        <p className="text-gray-700 mb-2"><strong>Telefono Host:</strong> {host_phone}</p>
+      </div>
+      <div className="mx-4 md:mx-20 mt-10">
+        <div className="flex items-center justify-between mb-3">
+          <h3 className="text-2xl font-bold">Recensioni:</h3>
+          <button
+            onClick={() => setIsModalOpen(true)}
+            className="px-2 py-2 ms-1 text-xs text-stone-600 bg-yellow-500 border-stone-400 rounded-xl cursor-pointer"
+          >
+            Aggiungi una recensione
+          </button>
+        </div>
+        <div className="flex flex-col">
+          {reviews?.length > 0 ? (
+            reviews.map((review) => (
+              <div className="m-2 w-full" key={review.id}>
+                <ReviewCard review={review} />
+              </div>
+            ))
+          ) : (
+            <p className="text-gray-700">Nessuna recensione disponibile</p>
+          )}
+        </div>
       </div>
       {isModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center backdrop-blur">
